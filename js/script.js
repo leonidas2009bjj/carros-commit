39ca7348221e1fa6d8f6821ab61ac416617633ed
leonidas2009BJJ <barrosleonidas948@gmail.com>
2025-12-15 13:18:48 -0300

    versão 4.2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
   menu.classList.toggle("move");
 };
+
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+// mantém o tema salvo
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+  toggleBtn.innerHTML = '<i class="bx bx-sun"></i>';
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.innerHTML = '<i class="bx bx-sun"></i>';
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.innerHTML = '<i class="bx bx-moon"></i>';
+  }
+});
